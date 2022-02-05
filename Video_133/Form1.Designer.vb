615d2d21226bad4dbac658dda21e7f197a042818
Nikoloz Chapichadze <nikoloz.chapichadze@gmail.com>
2022-02-05 17:32:54 -0500

    finished 137

--- Video_133/Form1.Designer.vb
+++ Video_133/Form1.Designer.vb
@@ -22,17 +22,41 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.btnAdd = New System.Windows.Forms.Button()
         Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 15
+        Me.ListBox1.Location = New System.Drawing.Point(12, 12)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(169, 214)
+        Me.ListBox1.TabIndex = 0
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(187, 12)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(105, 36)
+        Me.btnAdd.TabIndex = 1
+        Me.btnAdd.Text = "Add All"
+        Me.btnAdd.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(304, 243)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.ListBox1)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
 
     End Sub
 
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents btnAdd As Button
 End Class
--- Video_133/Form1.vb
+++ Video_133/Form1.vb
@@ -1,3 +1,22 @@
 ﻿Public Class Form1
 
+    Private myArray() As Integer = {5, 4, 3, 2, 1}
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        Dim index As Integer
+        Try
+            For index = 0 To 5
+                'If index >= myArray.Length Then
+                '    Throw New IndexOutOfRangeException("Error text")
+                'End If
+                ListBox1.Items.Add(myArray(index))
+            Next
+        Catch ex As IndexOutOfRangeException When index = 5
+            'MsgBox(ex.Message)
+            MessageBox.Show("Error message. index out of bounds")
+        Finally
+            MessageBox.Show("I am the finally")
+            Beep()
+        End Try
+    End Sub
 End Class
